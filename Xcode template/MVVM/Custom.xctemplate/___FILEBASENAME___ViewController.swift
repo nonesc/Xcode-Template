a68f420f61bc43fd4bae28498ad74578aa3e1081
nonesc <sujinchaichanamongkol@gmail.com>
2020-05-21 12:15:27 +0700

    mvvm template

---
+++ Xcode template/MVVM/Custom.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import UIKit
+
+class ___FILEBASENAME___: BaseViewController<___FILEBASENAME___ViewModelType> {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ Xcode template/MVVM/Custom.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol ___FILEBASENAME___ViewModelType {
+
+}
+
+protocol ___FILEBASENAME___ViewModelInput {
+
+}
+
+protocol ___FILEBASENAME___ViewModelOutput {
+
+
+}
+
+final class ___FILEBASENAME___ViewModel: ___FILEBASENAME___ViewModelType, ___FILEBASENAME___ViewModelInput, ___FILEBASENAME___ViewModelOutput {
+
+
+}
